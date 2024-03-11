26f3b94b2c93b5b8b680f0601da003ebf698c1e4
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-03-11 19:08:38 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -9,10 +9,13 @@ st.set_page_config(page_title="Marvik - Will", page_icon="🤖")
 st.title('Wilber Jiménez (Will)´s Test')
 st.title('Marvik`s Candidate 🤖')
 
-#TEST1
-st.header("Time Series - Google Colab:")
+#TEST 1
+st.header("Test 1: Time Series")
+st.link_button("Go to de Google Colab", "https://colab.research.google.com/drive/1q2A1iOXbjCaWFUSPjI07TgT7INmfKBYA#scrollTo=8gVfdvyk6xaK")
 
+#TEST 2
 
+st.header("Test 2: FAST API")
 # URL base de la API FastAPI
 base_url = "http://localhost:8000"
 
@@ -55,3 +58,9 @@ if st.button("Obtener Fecha"):
 
 # Mostrar el contador de llamadas
 obtener_contador_llamadas()
+
+
+#TEST 3
+st.header("Test 3: Style-Based Desings - Summary")
+st.write("The only constant is change. Some of the NVIDIA Research team presented in 2019 how the traditional way for architechure Generative Adversarial Networks can be improved in its performance and quality through new methods")
+st.write("")
